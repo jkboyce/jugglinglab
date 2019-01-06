7a61acf0c6028c58898918a9349d8a144758e70e
Jack Boyce <jboyce@gmail.com>
2019-01-06 13:25:17 -0800

    inserted quotes in case of spaces in paths

--- bin/jlab.bat
+++ bin/jlab.bat
@@ -10,13 +10,13 @@ SET "JL_BAT_DIR=%~dp0"
 SET "JL_WORKING_DIR=%cd%"
 
 IF EXIST "%JL_BAT_DIR%Juggling Lab.exe" (
-    SET JL_EXE="%JL_BAT_DIR%Juggling Lab.exe"
+    SET "JL_EXE=%JL_BAT_DIR%Juggling Lab.exe"
 ) ELSE (
     REM if script has been moved, look for Juggling Lab executable in its usual
     REM install location
 
     IF EXIST "C:\Program Files\Juggling Lab\Juggling Lab.exe" (
-        SET JL_EXE="C:\Program Files\Juggling Lab\Juggling Lab.exe"
+        SET "JL_EXE=C:\Program Files\Juggling Lab\Juggling Lab.exe"
     )
 )
 
@@ -28,14 +28,14 @@ IF DEFINED JL_EXE (
 
     IF "%1"=="" (
         REM if no arguments then print a help message
-        %JL_EXE% help >%TEMP%\jugglinglab_out.txt
+        "%JL_EXE%" help >"%TEMP%\jugglinglab_out.txt"
     ) ELSE (
         REM otherwise pass the args to Juggling Lab
-        %JL_EXE% %* >%TEMP%\jugglinglab_out.txt
+        "%JL_EXE%" %* >"%TEMP%\jugglinglab_out.txt"
     )
 
-    type %TEMP%\jugglinglab_out.txt
-    del %TEMP%\jugglinglab_out.txt
+    type "%TEMP%\jugglinglab_out.txt"
+    del "%TEMP%\jugglinglab_out.txt"
 ) ELSE (
     ECHO "Juggling Lab.exe" not found in same directory as this script.
 )
