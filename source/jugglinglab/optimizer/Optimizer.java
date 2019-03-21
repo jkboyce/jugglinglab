37af3c2bc6d0b9e27ec9732a3914ee4422b351f1
Jack Boyce <jboyce@gmail.com>
2019-03-20 23:21:56 -0700

    code cleanup

--- source/jugglinglab/optimizer/Optimizer.java
+++ source/jugglinglab/optimizer/Optimizer.java
@@ -1,24 +1,6 @@
 // Optimizer.java
 //
-// Copyright 2011 by Jack Boyce (jboyce@users.sourceforge.net) and others
-
-/*
-    This file is part of Juggling Lab.
-
-    Juggling Lab is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    Juggling Lab is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Juggling Lab; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+// Copyright 2019 by Jack Boyce (jboyce@gmail.com)
 
 package jugglinglab.optimizer;
 
@@ -43,12 +25,8 @@ import jugglinglab.core.*;
 
 
 public class Optimizer {
-    // static ResourceBundle guistrings;
-    static ResourceBundle errorstrings;
-    static {
-        // guistrings = JLLocale.getBundle("GUIStrings");
-        errorstrings = JLLocale.getBundle("ErrorStrings");
-    }
+    static final ResourceBundle guistrings = jugglinglab.JugglingLab.guistrings;
+    static final ResourceBundle errorstrings = jugglinglab.JugglingLab.errorstrings;
 
 	protected JMLPattern		pat;
 	protected MarginEquations	me;
