85b1fdbd8f0c41400222bae03023b60cf3493659
Jack Boyce <jboyce@gmail.com>
2022-12-03 15:56:19 -0800

    run from JugglingLab.jar file if present

--- bin/jlab.bat
+++ bin/jlab.bat
@@ -9,12 +9,30 @@ SET "JL_EXE="
 SET "JL_BAT_DIR=%~dp0"
 SET "JL_WORKING_DIR=%cd%"
 
-IF EXIST "%JL_BAT_DIR%Juggling Lab.exe" (
-    SET "JL_EXE=%JL_BAT_DIR%Juggling Lab.exe"
-) ELSE (
-    REM if script has been moved, look for Juggling Lab executable in its usual
-    REM install location
+REM First try to run JugglingLab.jar using system java installation.
 
+IF EXIST "%JL_BAT_DIR%\JugglingLab.jar" (
+    IF EXIST "%JAVA_HOME%\bin\java.exe" (
+        IF "%1"=="" (
+            REM if no arguments then print a help message
+            "%JAVA_HOME%\bin\java.exe" -cp "%JL_BAT_DIR%\JugglingLab.jar" jugglinglab.JugglingLab help
+        ) ELSE (
+            REM otherwise pass the args to Juggling Lab
+            "%JAVA_HOME%\bin\java.exe" -cp "%JL_BAT_DIR%\JugglingLab.jar" -Xss2048k -Djava.library.path=ortools-lib\ortools-win32-x86-64 jugglinglab.JugglingLab %*
+        )
+
+        SET "JL_EXE="
+        SET "JL_BAT_DIR="
+        SET "JL_WORKING_DIR="
+        EXIT /B
+    )
+)
+
+REM Otherwise, run using the installed Juggling Lab Windows binary
+
+IF EXIST "%JL_BAT_DIR%\Juggling Lab.exe" (
+    SET "JL_EXE=%JL_BAT_DIR%\Juggling Lab.exe"
+) ELSE (
     IF EXIST "C:\Program Files\Juggling Lab\Juggling Lab.exe" (
         SET "JL_EXE=C:\Program Files\Juggling Lab\Juggling Lab.exe"
     )
@@ -34,8 +52,8 @@ IF DEFINED JL_EXE (
         "%JL_EXE%" %* >"%TEMP%\jugglinglab_out.txt"
     )
 
-    type "%TEMP%\jugglinglab_out.txt"
-    del "%TEMP%\jugglinglab_out.txt"
+    TYPE "%TEMP%\jugglinglab_out.txt"
+    DEL "%TEMP%\jugglinglab_out.txt"
 ) ELSE (
     ECHO "Juggling Lab.exe" not found on this computer.
 )
