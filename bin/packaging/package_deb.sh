3443a03ec07f70acdebe1621d1863ab240070d48
Jack Boyce <jboyce@gmail.com>
2022-12-06 01:18:05 -0800

    add packaging for Debian Linux

---
+++ bin/packaging/package_deb.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Juggling Lab Debian Linux application packager
+#
+# Copyright 2022 by Jack Boyce and the Juggling Lab contributors
+# Released under the GNU General Public License v2
+#
+# -----------------------------------------------------------------------------
+#
+# This script packages Juggling Lab into a standalone Debian application
+#
+# It:
+#    - Builds the package "juggling-lab_<version>_amd64.deb" in the bin/ directory
+#
+# Note:
+#    - JugglingLab.jar needs to be built prior to running this, using Maven
+#    - Need to be using JDK 16 or later for jpackage to work
+#
+# Documentation at:
+#    https://docs.oracle.com/en/java/javase/17/jpackage/packaging-overview.html
+#    https://docs.oracle.com/en/java/javase/17/docs/specs/man/jpackage.html
+# Note: the "-Xss2048k" JVM argument is needed for Google OR-Tools to work
+
+cd ..
+mkdir target
+cp JugglingLab.jar target
+cp -r ortools-lib/ortools-linux-x86-64/* target
+
+jpackage \
+   --input target/ \
+   --name "Juggling Lab" \
+   --linux-package-name "juggling-lab" \
+   --app-version "1.6.3" \
+   --main-jar JugglingLab.jar \
+   --resource-dir "packaging/debian/" \
+   --java-options -Xss2048k \
+   --java-options -DJL_run_as_bundle=true \
+   --java-options -Djava.library.path=\$APPDIR \
+   --verbose
+
+rm -r target
+
+#find . -name "Juggling Lab*.dmg" -type f \
+#   -exec bash -c 'rm "${0/Juggling Lab/JugglingLab}"; mv "$0" "${0/Juggling Lab/JugglingLab}"' {} \;
+
