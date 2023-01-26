c74de21d2e0219a2e0e86826889b132e003de6ad
Jack Boyce <jboyce@gmail.com>
2023-01-25 22:35:08 -0800

    add Windows launcher script

---
+++ bin/JugglingLab.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+
+REM Windows launcher for Juggling Lab application
+REM
+REM Copyright 2002-2023 Jack Boyce and the Juggling Lab contributors
+REM Released under the GNU General Public License v2
+
+SET "JL_BAT_DIR=%~dp0"
+
+REM Try to launch JugglingLab.jar using the system Java installation
+
+IF EXIST "%JL_BAT_DIR%\JugglingLab.jar" (
+    IF EXIST "%JAVA_HOME%\bin\java.exe" (
+        "%JAVA_HOME%\bin\java.exe" -cp "%JL_BAT_DIR%\JugglingLab.jar" -Xss2048k -Djava.library.path="%JL_BAT_DIR%\ortools-lib\ortools-win32-x86-64" jugglinglab.JugglingLab
+    ) ELSE (
+        ECHO Java not found. Install Java 11 or higher and ensure JAVA_HOME
+        ECHO environment variable is set.
+    )
+) ELSE (
+    ECHO "JugglingLab.jar" not found in same directory as this script.
+)
+
+SET "JL_BAT_DIR="
