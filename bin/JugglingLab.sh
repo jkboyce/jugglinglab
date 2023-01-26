795f891fa5cb607dafa0e22936cb57c0061dabf5
Jack Boyce <jboyce@gmail.com>
2023-01-25 22:34:40 -0800

    add macOS support to application launcher

--- bin/JugglingLab.sh
+++ bin/JugglingLab.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 #
-# Command line launcher for Juggling Lab application
+# Unix (macOS/Linux) launcher for Juggling Lab application
 #
-# Copyright 2002-2022 Jack Boyce and the Juggling Lab contributors
+# Copyright 2002-2023 Jack Boyce and the Juggling Lab contributors
 # Released under the GNU General Public License v2
 
 
 DIR=$(dirname "$0")
-export JL_WORKING_DIR="`pwd`"
 
-# Make sure an adequate version of Java is installed
+# Check if an adequate version of Java is installed
 if type java >/dev/null 2>&1; then
     _java=java
 elif [[ -n "${JAVA_HOME}" ]] && [[ -x "${JAVA_HOME}/bin/java" ]]; then
     _java="${JAVA_HOME}/bin/java"
 else
-    echo >&2 "Java not found. Be sure Java 11 or higher is installed and \$JAVA_HOME"
+    echo >&2 "Java not found. Install Java 11 or higher and ensure \$JAVA_HOME"
     echo >&2 "environment variable is set."
     exit 1
 fi
@@ -33,9 +32,12 @@ fi
 JL_JAR="${DIR}/JugglingLab.jar"
 
 if [ -a "${JL_JAR}" ]; then
-    "$_java" -cp "${JL_JAR}" -Xss2048k -Djava.library.path="${DIR}/ortools-lib/ortools-linux-x86-64" jugglinglab.JugglingLab
-    exit 1
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # include both Arm and x86 versions of OR-Tools; JRE will choose correct one at runtime
+        "$_java" -cp "${JL_JAR}" -Xss2048k -Djava.library.path="${DIR}/ortools-lib/ortools-darwin-x86-64:${DIR}/ortools-lib/ortools-darwin-aarch64" jugglinglab.JugglingLab
+    else
+        "$_java" -cp "${JL_JAR}" -Xss2048k -Djava.library.path="${DIR}/ortools-lib/ortools-linux-x86-64:${DIR}/ortools-lib/ortools-linux-aarch64" jugglinglab.JugglingLab
+    fi
 else
     echo >&2 "\"JugglingLab.jar\" not found in same directory as this script."
-    exit 1
 fi
