f071f74824f6d60b3ffda832d09758dd65990458
Jack Boyce <jboyce@gmail.com>
2025-10-23 17:19:10 -0700

    convert Prop classes to Kotlin

---
+++ src/main/kotlin/jugglinglab/prop/BallProp.kt
@@ -0,0 +1,313 @@
+//
+// BallProp.java
+//
+// Copyright 2002-2025 Jack Boyce and the Juggling Lab contributors
+//
+
+package jugglinglab.prop
+
+import jugglinglab.JugglingLab.errorstrings
+import jugglinglab.util.Coordinate
+import jugglinglab.util.JuggleExceptionUser
+import jugglinglab.util.ParameterDescriptor
+import jugglinglab.util.ParameterList
+import jugglinglab.util.parseDouble
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.Image
+import java.awt.image.BufferedImage
+import java.text.MessageFormat
+import java.util.*
+import kotlin.math.max
+import kotlin.math.min
+
+class BallProp : Prop() {
+    private var diam: Double = DIAM_DEF // diameter, in cm
+    private var color: Color = COLOR_DEF
+    private var colornum: Int = COLORNUM_DEF
+    private var highlight: Boolean = HIGHLIGHT_DEF
+
+    private var ballimage: BufferedImage? = null
+    private var lastzoom: Double = 0.0
+    private var size: Dimension? = null
+    private var center: Dimension? = null
+    private var grip: Dimension? = null
+
+    override val type = "Ball"
+
+    override fun getEditorColor(): Color {
+        return color
+    }
+
+    override fun getParameterDescriptors(): Array<ParameterDescriptor> {
+        val result = ArrayList<ParameterDescriptor>()
+        val colorRange = ArrayList<String>()
+        Collections.addAll(colorRange, *COLOR_NAMES)
+        result.add(
+            ParameterDescriptor(
+                "color",
+                ParameterDescriptor.TYPE_CHOICE,
+                colorRange,
+                COLOR_NAMES[COLORNUM_DEF],
+                COLOR_NAMES[colornum]
+            )
+        )
+        result.add(
+            ParameterDescriptor(
+                "diam",
+                ParameterDescriptor.TYPE_FLOAT,
+                null,
+                DIAM_DEF,
+                diam
+            )
+        )
+        result.add(
+            ParameterDescriptor(
+                "highlight",
+                ParameterDescriptor.TYPE_BOOLEAN,
+                null,
+                HIGHLIGHT_DEF,
+                highlight
+            )
+        )
+
+        return result.toTypedArray()
+    }
+
+    @Throws(JuggleExceptionUser::class)
+    override fun init(st: String?) {
+        if (st == null) {
+            return
+        }
+        val pl = ParameterList(st)
+
+        val colorstr = pl.getParameter("color")
+        if (colorstr != null) {
+            var temp: Color? = null
+
+            if (colorstr.indexOf(',') == -1) {  // color name
+                for (i in COLOR_NAMES.indices) {
+                    if (COLOR_NAMES[i].equals(colorstr, ignoreCase = true)) {
+                        temp = COLOR_VALS[i]
+                        colornum = i
+                        break
+                    }
+                }
+            } else {  // RGB or RGBA
+                // delete the '{' and '}' characters first
+                var str: String? = colorstr
+                var pos: Int
+                while ((str!!.indexOf('{').also { pos = it }) >= 0) {
+                    str = str.take(pos) + str.substring(pos + 1)
+                }
+                while ((str!!.indexOf('}').also { pos = it }) >= 0) {
+                    str = str.take(pos) + str.substring(pos + 1)
+                }
+
+                val st2 = StringTokenizer(str, ",", false)
+                val tokens = st2.countTokens()
+
+                if (tokens == 3 || tokens == 4) {
+                    val red: Int
+                    val green: Int
+                    val blue: Int
+                    var alpha = 255
+                    var token: String? = null
+                    try {
+                        token = st2.nextToken().trim { it <= ' ' }
+                        red = token.toInt()
+                        token = st2.nextToken().trim { it <= ' ' }
+                        green = token.toInt()
+                        token = st2.nextToken().trim { it <= ' ' }
+                        blue = token.toInt()
+                        if (tokens == 4) {
+                            token = st2.nextToken().trim { it <= ' ' }
+                            alpha = token.toInt()
+                        }
+                    } catch (_: NumberFormatException) {
+                        val template: String = errorstrings.getString("Error_number_format")
+                        val arguments = arrayOf<Any?>(token)
+                        throw JuggleExceptionUser(
+                            "Ball prop color: " + MessageFormat.format(template, *arguments)
+                        )
+                    }
+                    temp = Color(red, green, blue, alpha)
+                } else {
+                    throw JuggleExceptionUser(
+                        "Ball prop color: " + errorstrings.getString("Error_token_count")
+                    )
+                }
+            }
+
+            if (temp != null) {
+                color = temp
+            } else {
+                val template: String = errorstrings.getString("Error_prop_color")
+                val arguments = arrayOf<Any?>(colorstr)
+                throw JuggleExceptionUser(MessageFormat.format(template, *arguments))
+            }
+        }
+
+        val diamstr = pl.getParameter("diam")
+        if (diamstr != null) {
+            try {
+                val temp = parseDouble(diamstr.trim { it <= ' ' })
+                if (temp > 0) {
+                    diam = temp
+                } else {
+                    throw JuggleExceptionUser(errorstrings.getString("Error_prop_diameter"))
+                }
+            } catch (_: NumberFormatException) {
+                val template: String = errorstrings.getString("Error_number_format")
+                val arguments = arrayOf<Any?>("diam")
+                throw JuggleExceptionUser(
+                    "Ball prop diameter: " + MessageFormat.format(template, *arguments)
+                )
+            }
+        }
+
+        val highlightstr = pl.getParameter("highlight")
+        if (highlightstr != null) {
+            highlight = highlightstr.toBoolean()
+        }
+    }
+
+    override fun getMax(): Coordinate {
+        return Coordinate(diam / 2, 0.0, diam / 2)
+    }
+
+    override fun getMin(): Coordinate {
+        return Coordinate(-diam / 2, 0.0, -diam / 2)
+    }
+
+    override fun getWidth(): Double {
+        return diam
+    }
+
+    override fun getProp2DImage(zoom: Double, camangle: DoubleArray): Image? {
+        if (ballimage == null || zoom != lastzoom) {
+            // first call or display resized?
+            recalc2D(zoom)
+        }
+        return ballimage
+    }
+
+    override fun getProp2DSize(zoom: Double): Dimension? {
+        if (size == null || zoom != lastzoom) {
+            // first call or display resized?
+            recalc2D(zoom)
+        }
+        return size
+    }
+
+    override fun getProp2DCenter(zoom: Double): Dimension? {
+        if (center == null || zoom != lastzoom) {
+            recalc2D(zoom)
+        }
+        return center
+    }
+
+    override fun getProp2DGrip(zoom: Double): Dimension? {
+        if (grip == null || zoom != lastzoom) {
+            // first call or display resized?
+            recalc2D(zoom)
+        }
+        return grip
+    }
+
+    private fun recalc2D(zoom: Double) {
+        var ballPixelSize = (0.5 + zoom * diam).toInt()
+        ballPixelSize = max(ballPixelSize, 1)
+
+        // create a ball image of diameter ball_pixel_size, and transparent background
+        ballimage = BufferedImage(
+            ballPixelSize + 1, ballPixelSize + 1, BufferedImage.TYPE_INT_ARGB_PRE
+        )
+        val ballg = ballimage!!.createGraphics()
+
+        /*
+        ballg.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                               RenderingHints.VALUE_ANTIALIAS_ON);
+        */
+        if (highlight) {
+            val highlightOvals = ballPixelSize / 1.2f // Number of concentric circles to draw.
+            val rgb = FloatArray(4)
+            rgb[0] = color.red.toFloat() / 255f
+            rgb[1] = color.green.toFloat() / 255f
+            rgb[2] = color.blue.toFloat() / 255f
+            rgb[3] = color.alpha.toFloat() / 255f
+
+            // make the color a little darker so that there is some contrast
+            for (i in 0..2) {
+                rgb[i] = rgb[i] / 2.5f
+            }
+
+            ballg.color = Color(rgb[0], rgb[1], rgb[2], rgb[3])
+            ballg.fillOval(0, 0, ballPixelSize, ballPixelSize) // Full sized ellipse.
+
+            // draw the highlight on the ball
+            var i = 0
+            while (i < highlightOvals) {
+                // Calculate the new color
+                for (j in 0..2) {
+                    rgb[j] = min(rgb[j] + (1f / highlightOvals), 1f)
+                }
+                ballg.color = Color(rgb[0], rgb[1], rgb[2], rgb[3])
+                ballg.fillOval(
+                    (i / 1.1).toInt(),
+                    (i / 2.5).toInt(),  // literals control how fast highlight
+                    // moves right and down respectively.
+                    ballPixelSize - (i * 1.3).toInt(),  // these control how fast the
+                    ballPixelSize - (i * 1.3).toInt()
+                ) // highlight converges to a point.
+                i++
+            }
+        } else {
+            ballg.color = color
+            ballg.fillOval(0, 0, ballPixelSize, ballPixelSize)
+        }
+
+        size = Dimension(ballPixelSize, ballPixelSize)
+        center = Dimension(ballPixelSize / 2, ballPixelSize / 2)
+        grip = Dimension(ballPixelSize / 2, ballPixelSize / 2)
+
+        lastzoom = zoom
+    }
+
+    companion object {
+        private val COLOR_NAMES: Array<String> = arrayOf<String>(
+            "transparent",
+            "black",
+            "blue",
+            "cyan",
+            "gray",
+            "green",
+            "magenta",
+            "orange",
+            "pink",
+            "red",
+            "white",
+            "yellow",
+        )
+
+        private val COLOR_VALS: Array<Color> = arrayOf<Color>(
+            Color(0, 0, 0, 0),
+            Color.black,
+            Color.blue,
+            Color.cyan,
+            Color.gray,
+            Color.green,
+            Color.magenta,
+            Color.orange,
+            Color.pink,
+            Color.red,
+            Color.white,
+            Color.yellow,
+        )
+
+        private val COLOR_DEF: Color = Color.red
+        private const val COLORNUM_DEF: Int = 9  // red
+        private const val DIAM_DEF: Double = 10.0  // in cm
+        private const val HIGHLIGHT_DEF: Boolean = false
+    }
+}
---
+++ src/main/kotlin/jugglinglab/prop/ImageProp.kt
@@ -0,0 +1,220 @@
+//
+// ImageProp.kt
+//
+// Copyright 2002-2025 Jack Boyce and the Juggling Lab contributors
+//
+
+package jugglinglab.prop
+
+import jugglinglab.JugglingLab.errorstrings
+import jugglinglab.util.Coordinate
+import jugglinglab.util.JuggleExceptionUser
+import jugglinglab.util.ParameterDescriptor
+import jugglinglab.util.ParameterList
+import jugglinglab.util.parseDouble
+import java.awt.*
+import java.awt.image.BufferedImage
+import java.io.IOException
+import java.net.MalformedURLException
+import java.net.URI
+import java.net.URISyntaxException
+import java.net.URL
+import java.text.MessageFormat
+import javax.imageio.ImageIO
+
+class ImageProp : Prop() {
+    private var url: URL?
+    private var image: BufferedImage? = null
+    private var scaledImage: BufferedImage? = null
+    private var width: Double = 0.0
+    private var height: Double = 0.0
+    private var size: Dimension? = null
+    private var center: Dimension? = null
+    private var grip: Dimension? = null
+    private var lastZoom = 0.0
+
+    init {
+        if (imageUrlDefault == null) {
+            throw JuggleExceptionUser("ImageProp error: Default image not set")
+        }
+        url = imageUrlDefault
+        loadImage()
+        rescaleImage(1.0)
+    }
+
+    @Throws(JuggleExceptionUser::class)
+    private fun loadImage() {
+        try {
+            val mt = MediaTracker(object : Component() {})
+            image = ImageIO.read(url)
+            mt.addImage(image, 0)
+            // Try to laod the image
+            try {
+                mt.waitForAll()
+            } catch (_: InterruptedException) {
+            }
+
+            if (mt.isErrorAny()) {
+                image = null
+                // This could also be bad image data, but it is usually a nonexistent file.
+                throw JuggleExceptionUser(errorstrings.getString("Error_bad_file"))
+            }
+
+            val aspectRatio = (image!!.height.toDouble()) / (image!!.width.toDouble())
+            width = WIDTH_DEF
+            height = WIDTH_DEF * aspectRatio
+        } catch (_: IOException) {
+            throw JuggleExceptionUser(errorstrings.getString("Error_bad_file"))
+        } catch (_: SecurityException) {
+            throw JuggleExceptionUser(errorstrings.getString("Error_security_restriction"))
+        }
+    }
+
+    private fun rescaleImage(zoom: Double) {
+        var imagePixelWidth = (0.5 + zoom * width).toInt()
+        if (imagePixelWidth < 1) {
+            imagePixelWidth = 1
+        }
+        var imagePixelHeight = (0.5 + zoom * height).toInt()
+        if (imagePixelHeight < 1) {
+            imagePixelHeight = 1
+        }
+        size = Dimension(imagePixelWidth, imagePixelHeight)
+        center = Dimension(imagePixelWidth / 2, imagePixelHeight / 2)
+
+        val offsetx = imagePixelWidth / 2
+        val offsety = imagePixelHeight
+        grip = Dimension(offsetx, offsety)
+
+        lastZoom = zoom
+
+        scaledImage = BufferedImage(imagePixelWidth, imagePixelHeight, image!!.type)
+        val g = scaledImage!!.createGraphics()
+        g.setRenderingHint(
+            RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR
+        )
+        g.drawImage(
+            image,
+            0,
+            0,
+            imagePixelWidth,
+            imagePixelHeight,
+            0,
+            0,
+            image!!.width,
+            image!!.height,
+            null
+        )
+        g.dispose()
+    }
+
+    override val type = "Image"
+
+    override fun getEditorColor(): Color {
+        // The color that shows up in the visual editor
+        // We could try to get an average color for the image
+        return Color.white
+    }
+
+    override fun getParameterDescriptors(): Array<ParameterDescriptor> {
+        val result = ArrayList<ParameterDescriptor>()
+        result.add(
+            ParameterDescriptor(
+                "image", ParameterDescriptor.TYPE_ICON, null, imageUrlDefault, url
+            )
+        )
+        result.add(
+            ParameterDescriptor(
+                "width",
+                ParameterDescriptor.TYPE_FLOAT,
+                null,
+                WIDTH_DEF,
+                width
+            )
+        )
+        return result.toTypedArray()
+    }
+
+    @Throws(JuggleExceptionUser::class)
+    override fun init(st: String?) {
+        if (st == null) {
+            return
+        }
+        val pl = ParameterList(st)
+
+        val sourcestr = pl.getParameter("image")
+        if (sourcestr != null) {
+            try {
+                url = URI(sourcestr).toURL()
+                loadImage()
+            } catch (_: URISyntaxException) {
+                throw JuggleExceptionUser(errorstrings.getString("Error_malformed_URL"))
+            } catch (_: MalformedURLException) {
+                throw JuggleExceptionUser(errorstrings.getString("Error_malformed_URL"))
+            }
+        }
+
+        val widthstr = pl.getParameter("width")
+        if (widthstr != null) {
+            try {
+                val temp = parseDouble(widthstr)
+                if (temp > 0) {
+                    width = temp
+                    val aspectRatio = (image!!.getHeight(null).toDouble()) / (image!!.getWidth(null).toDouble())
+                    height = width * aspectRatio
+                } else {
+                    throw NumberFormatException()
+                }
+            } catch (_: NumberFormatException) {
+                val template: String = errorstrings.getString("Error_number_format")
+                val arguments = arrayOf<Any?>("width")
+                throw JuggleExceptionUser(MessageFormat.format(template, *arguments))
+            }
+        }
+    }
+
+    override fun getProp2DImage(zoom: Double, camangle: DoubleArray): Image? {
+        if (zoom != lastZoom) {
+            rescaleImage(zoom)
+        }
+        return scaledImage
+    }
+
+    override fun getMax(): Coordinate {
+        return Coordinate(width / 2, 0.0, width)
+    }
+
+    override fun getMin(): Coordinate {
+        return Coordinate(-width / 2, 0.0, 0.0)
+    }
+
+    override fun getWidth(): Double {
+        return width
+    }
+
+    override fun getProp2DSize(zoom: Double): Dimension? {
+        if (size == null || zoom != lastZoom) {
+            rescaleImage(zoom)
+        }
+        return size
+    }
+
+    override fun getProp2DCenter(zoom: Double): Dimension? {
+        if (center == null || zoom != lastZoom) {
+            rescaleImage(zoom)
+        }
+        return center
+    }
+
+    override fun getProp2DGrip(zoom: Double): Dimension? {
+        if (grip == null || zoom != lastZoom) {
+            rescaleImage(zoom)
+        }
+        return grip
+    }
+
+    companion object {
+        private const val WIDTH_DEF: Double = 10.0  // in centimeters
+        private var imageUrlDefault: URL? = ImageProp::class.java.getResource("/ball.png")
+    }
+}
