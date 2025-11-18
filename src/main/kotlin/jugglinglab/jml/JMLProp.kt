d2f4b96987ce83271142595b641dcf44714aee52
Jack Boyce <jboyce@gmail.com>
2025-11-18 01:32:36 -0800

    refactor prop classes

--- src/main/kotlin/jugglinglab/jml/JMLProp.kt
+++ src/main/kotlin/jugglinglab/jml/JMLProp.kt
@@ -11,22 +11,23 @@ import jugglinglab.util.JuggleExceptionUser
 import java.io.IOException
 import java.io.PrintWriter
 
-data class JMLProp @Throws(JuggleExceptionUser::class) constructor(
+data class JMLProp (
     val type: String,
     val mod: String?
 ) {
     // constructor for reading JML
     @Suppress("unused")
-    @Throws(JuggleExceptionUser::class)
     constructor(current: JMLNode, version: String?) : this(
         type = current.attributes.getAttribute("type")!!,
         mod = current.attributes.getAttribute("mod")
     )
 
+    @get:Throws(JuggleExceptionUser::class)
     val prop: Prop by lazy {
         Prop.newProp(type).apply { initProp(mod) }
     }
     
+    @get:Throws(JuggleExceptionUser::class)
     val isColorable: Boolean
         get() = prop.isColorable
 
