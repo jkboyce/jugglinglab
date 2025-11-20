03dc62bcff21159dbd7329f91a29724ea10f7871
Jack Boyce <jboyce@gmail.com>
2025-11-20 10:57:24 -0800

    add siteswap parsing test cases

--- src/test/kotlin/jugglinglab/notation/SiteswapPatternTest.kt
+++ src/test/kotlin/jugglinglab/notation/SiteswapPatternTest.kt
@@ -18,4 +18,53 @@ class SiteswapPatternTest {
         assertEquals(1, pattern.numberOfJugglers)
         assertEquals(6, pattern.numberOfPaths)
     }
+
+    @Test
+    fun `pattern parsing thrown 2`() {
+        val pattern = SiteswapPattern().fromString("(4,3x)!(2,0)!(3x,0)!")
+        assertEquals(1, pattern.numberOfJugglers)
+        assertEquals(4, pattern.numberOfPaths)
+    }
+
+    @Test
+    fun `pattern parsing short beats`() {
+        val pattern = SiteswapPattern().fromString("(0,6x)!(0,0)!(6x,0)!(0,0)!")
+        assertEquals(1, pattern.numberOfJugglers)
+        assertEquals(3, pattern.numberOfPaths)
+    }
+
+    @Test
+    fun `pattern parsing mixed sync async`() {
+        val pattern = SiteswapPattern().fromString("4x1(4x,3x)*")
+        assertEquals(1, pattern.numberOfJugglers)
+        assertEquals(3, pattern.numberOfPaths)
+    }
+
+    @Test
+    fun `pattern parsing squeeze pattern`() {
+        val pattern = SiteswapPattern().fromString("([42],4x)*")
+        assertEquals(1, pattern.numberOfJugglers)
+        assertEquals(5, pattern.numberOfPaths)
+    }
+
+    @Test
+    fun `pattern parsing sync async transition`() {
+        val pattern = SiteswapPattern().fromString("(645^2)65x6x1x((6x,4)*^2)(7,5x)(4,1x)!")
+        assertEquals(1, pattern.numberOfJugglers)
+        assertEquals(5, pattern.numberOfPaths)
+    }
+
+    @Test
+    fun `pattern parsing passing 1`() {
+        val pattern = SiteswapPattern().fromString("<([2xp/2x],[2xp/2])|(2,[2/2xp])><(2,[2p/2])|([2/2p],[2/2p])>")
+        assertEquals(2, pattern.numberOfJugglers)
+        assertEquals(7, pattern.numberOfPaths)
+    }
+
+    @Test
+    fun `pattern parsing large throws`() {
+        val pattern = SiteswapPattern().fromString("{49}1")
+        assertEquals(1, pattern.numberOfJugglers)
+        assertEquals(25, pattern.numberOfPaths)
+    }
 }
