f54c868c8bffc3ae2812662c74e5dd2b844b2b7d
Jack Boyce <jboyce@gmail.com>
2025-11-30 01:19:00 -0800

    update button

--- src/commonMain/kotlin/jugglinglab/core/AboutContent.kt
+++ src/commonMain/kotlin/jugglinglab/core/AboutContent.kt
@@ -18,13 +18,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import jugglinglab.generated.resources.*
@@ -81,11 +79,8 @@ fun AboutContent(onCloseRequest: () -> Unit) {
             Button(
                 onClick = onCloseRequest,
                 modifier = Modifier.align(Alignment.End),
-                // Set colors to White background/Black text to match target.jpg
-                // This also helps resolve default font padding issues that clip text.
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
             ) {
-                Text(stringResource(Res.string.gui_ok), color = Color.Black)
+                Text(stringResource(Res.string.gui_ok))
             }
         }
     }
